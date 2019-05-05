dfdb8cb6b8cdd3331c0a3b643b3c8e506345a8c0
woody <hermes.conrad.pe@gmail.com>
2019-05-05 20:53:17 +0300

    completion hotkey, lsp, helper

---
+++ custom/setup-completion.el
@@ -0,0 +1,30 @@
+(provide 'setup-completion)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: company              ;;
+;;                               ;;
+;; GROUP: Convenience -> Company ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(use-package company)
+(add-hook 'after-init-hook 'global-company-mode)
+(use-package company-flx)
+(global-set-key [(control ?\,)] 'company-complete-common)
+;(eval-after-load 'company
+;  '(progn
+;;     (define-key company-active-map (kbd "C-,") 'completion-at-point)))
+;     (define-key company-active-map ("C-,") 'company-complete-common)))
+
+
+;(global-set-key "C-," 'company-complete-common)
+(setq company-idle-delay nil)
+
+(require 'color)
+(let ((bg (face-attribute 'default :background)))
+    (custom-set-faces
+     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
--- custom/setup-lsp.el
+++ custom/setup-lsp.el
@@ -5,8 +5,7 @@
 
 (use-package lsp-ui :commands lsp-ui-mode)
 (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+(setq lsp-ui-doc-delay 1)
 
 (use-package company-lsp :commands company-lsp)
 (push 'company-lsp company-backends)
@@ -14,3 +13,7 @@
 (use-package helm-lsp)
 
 (add-hook 'rust-mode-hook #'lsp)
+
+;(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+
