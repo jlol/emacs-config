76d7aafd53d1b9dd829a818f0bc799d7e0806d52
woody <hermes.conrad.pe@gmail.com>
2019-05-05 13:59:53 +0300

    Dashboard

--- custom/setup-convenience.el
+++ custom/setup-convenience.el
@@ -61,3 +61,34 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package expand-region)
 (global-set-key (kbd "M-m") 'er/expand-region)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; IBuffer
+;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package ibuffer-vc)
+(add-hook 'ibuffer-hook
+          (lambda ()
+            (ibuffer-vc-set-filter-groups-by-vc-root)
+            (unless (eq ibuffer-sorting-mode 'alphabetic)
+              (ibuffer-do-sort-by-alphabetic))))
+
+(setq ibuffer-formats
+      '((mark modified read-only vc-status-mini " "
+              (name 18 18 :left :elide)
+              " "
+              (size 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              (vc-status 16 16 :left)
+              " "
+              filename-and-process)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGES: projectile             ;;
+;;                                  ;;
+;; GROUP: Convenience -> Projectile ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package projectile)
+(projectile-global-mode)
