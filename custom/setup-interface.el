6ab5891f6aec169a54f2f88cacd806895c3fdced
woody <hermes.conrad.pe@gmail.com>
2019-05-05 14:04:39 +0300

    dashboard options

--- custom/setup-interface.el
+++ custom/setup-interface.el
@@ -9,3 +9,9 @@
   :ensure t
   :config
   (dashboard-setup-startup-hook))
+
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
