4b258fc3733b1873ec48947f56c5a4e79c58c921
woody <hermes.conrad.pe@gmail.com>
2019-05-05 18:37:30 +0300

    lsp, ivy improvements, etc.

---
+++ custom/setup-misc.el
@@ -0,0 +1,9 @@
+(provide 'setup-misc)
+
+(use-package helm-ag)
+
+(custom-set-variables
+ '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
+ '(helm-ag-command-option "--all-text")
+ '(helm-ag-insert-at-point 'symbol)
+ '(helm-ag-ignore-buffer-patterns '("\\.txt\\'" "\\.mkd\\'")))
