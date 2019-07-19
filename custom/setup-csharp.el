03577b5bcd6983656f9a93fdf83d6afb326cb6cf
Juan Luis Ordoñez <juanluis@lightneer.com>
2019-07-19 14:58:28 +0300

    csharp updates

--- custom/setup-csharp.el
+++ custom/setup-csharp.el
@@ -33,6 +33,8 @@
     ;(electric-pair-local-mode 1) ;; Emacs 25
 
     (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
-    (local-set-key (kbd "C-c C-c") 'recompile))
+    (local-set-key (kbd "C-c C-c") 'recompile)    
+    (global-set-key [(control ?\.)] 'company-omnisharp)
+)
 
 (add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
