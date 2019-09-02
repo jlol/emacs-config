316225ac1fe4a3f3925d17fd9a071470e7cc1a24
Juan Luis Ordoñez <juanluis@lightneer.com>
2019-09-02 17:55:23 +0300

    update

--- custom/setup-rust.el
+++ custom/setup-rust.el
@@ -4,8 +4,19 @@
 (use-package rust-auto-use)
 
 (use-package flycheck-rust)
-(with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+;(with-eval-after-load 'rust-mode
+;  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 (use-package cargo)
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
+;(add-hook 'rust-mode-hook 'cargo-minor-mode)
+
+;;;;;;;;;;;;;;;; test
+
+(defun my-rust-mode-setup ()
+    (with-eval-after-load 'rust-mode
+       (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+    (cargo-minor-mode)
+
+)
+
+(add-hook 'rust-mode-hook 'my-rust-mode-setup t)
