7103f48d0c4785d283b6c984ea3ad247c7748337
Juan Luis Ordoñez <juanluis@lightneer.com>
2019-09-04 11:35:23 +0300

    added snippet, fixed company for omnisharp

--- custom/setup-csharp.el
+++ custom/setup-csharp.el
@@ -17,7 +17,12 @@
 
 (defun my-csharp-mode-setup ()
     (omnisharp-mode)
+
+    (eval-after-load
+     'company
+     '(add-to-list 'company-backends 'company-omnisharp))
     (company-mode)
+
     (flycheck-mode)
 
     (setq indent-tabs-mode nil)
@@ -35,8 +40,9 @@
     (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
     (local-set-key (kbd "C-c C-c") 'recompile)    
     (global-set-key [(control ?\.)] 'company-omnisharp)
-    (global-set-key [ f12 ] 'omnisharp-go-to-definition)
-    (global-set-key [(control f12)] 'omnisharp-find-usages-with-ido)
+    (global-set-key [ f12 ] 'omnisharp-go-to-definition-other-window)
+    (global-set-key [ f9 ] 'omnisharp-go-to-definition)
+    (global-set-key [(control f12)] 'omnisharp-helm-find-usages)
     (global-set-key [(control f2)] 'omnisharp-rename)
     (local-set-key (kbd "C-c f") 'omnisharp-navigate-to-solution-member)
 )
