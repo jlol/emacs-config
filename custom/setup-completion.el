4e0fdafd03e96f0f974f1866b4c1abb27cd9bf98
Juan Luis Ordoñez <juanluis@lightneer.com>
2019-09-06 18:53:36 +0300

    removed auto-complete decoration messing with windows daemon

--- custom/setup-completion.el
+++ custom/setup-completion.el
@@ -20,11 +20,12 @@
 ;(global-set-key "C-," 'company-complete-common)
 (setq company-idle-delay nil)
 
-(require 'color)
-(let ((bg (face-attribute 'default :background)))
-    (custom-set-faces
-     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+;;(require 'color)
+;;(let ((bg (face-attribute 'default :background)))
+;;    (custom-set-faces
+;;     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+;;    `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+;;     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+;;     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+;;     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+;;     ))
